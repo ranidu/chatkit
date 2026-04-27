9b5c94680a86667e457b4dec74be50c4621604b8
Ranidu Nadeesha <ranidunadeesha@gmail.com>
2026-04-27 23:14:41 +0800

    chore: update package and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      include: ["src"],
+      exclude: ["dev"],
+    }),
+  ],
+  root: 'dev',
+  server: {
+    port: 3000,
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: () => `chatkit.mjs`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: "src",
+      },
+    },
+  },
+});
